2f46f0108b2f79d1a942f3bd8e2ce3e6ec219d18
Allan Feng <chunlinfeng0920@gmail.com>
2024-04-24 22:20:29 -0400

    Get local dataset from post data

--- get_db.py
+++ get_db.py
@@ -2,17 +2,35 @@ from redditData.google_search import search_by_time_period
 from redditData.reddit_api import Reddit_API
 import SQLjsonHelper
 
-def get_post_info(start_date = "20231203", end_date = "20240110"):
-    article_ids=search_by_time_period(start_date,end_date)
+def get_post_info(start_date="20231203", end_date="20240110"):
+    article_ids = search_by_time_period(start_date, end_date)
     print(article_ids)
-    api=Reddit_API()
+    api = Reddit_API()
 
-    for id in article_ids:
-        data=api.get_post_data(id,"1000")
-        # print(api.get_post_dict(data[0]))
-        SQLjsonHelper.display_table()
-        # dict={}
-        # api.get_dicts(data[0],dict,id,10)
-        # print(dict)
+    for article_id in article_ids:
+        post_data = api.get_post_data(article_id, "1000")
+        post_info = api.get_post_dict(post_data[0])
+
+        # Insert post information into the 'redditPosts' table
+        post_info_to_insert = {
+            "postId": post_info["id"],
+            "author": post_info["author"],
+            "likes": post_info["likes"]
+        }
+        SQLjsonHelper.add_post("redditPosts", post_info_to_insert)
+
+        # Insert comments into the 'comments' table
+        comments = post_info.get("comments", [])
+        for comment in comments:
+            comment_info_to_insert = {
+                "article_id": post_info["id"],
+                "username": comment["username"],
+                "content": comment["content"],
+                "parent_comment_id": comment.get("parent_comment_id", None),
+                "upvotes": comment["upvotes"],
+                "downvotes": comment["downvotes"]
+            }
+            SQLjsonHelper.add_comment("comments", comment_info_to_insert)
+
 if __name__ == "__main__":
     get_post_info()
